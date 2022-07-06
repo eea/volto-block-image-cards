09449d5a440ae3879acf40fdf8b69d427fe8a6e0
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-07-06 17:41:04 +0300

    Don't hardcode carousel as default

--- src/ImageCards/ImageCardsView.jsx
+++ src/ImageCards/ImageCardsView.jsx
@@ -3,13 +3,16 @@ import config from '@plone/volto/registry';
 
 const ImageCardView = (props) => {
   const byDisplayType = {};
-  const blockRenderers = config.blocks.blocksConfig.imagecards.blockRenderers;
+  const blockConfig = config.blocks.blocksConfig.imagecards;
+  const { blockRenderers, defaultRendererName } = blockConfig;
+
   const block_renderers_ids = Object.keys(blockRenderers);
   block_renderers_ids.forEach(function (value) {
     byDisplayType[value] = blockRenderers[value].view;
   });
 
-  const Impl = byDisplayType[props.data.display || 'carousel'];
+  const Impl = byDisplayType[props.data.display || defaultRendererName];
+
   return <Impl {...props} />;
 };
 
